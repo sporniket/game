a6728bd45cc6f8b04eff9b5d3a67a515654cb92b
David SPORN <sporniket.studio@gmail.com>
2016-08-05 06:55:32 +0200

    redefined class hierarchy and packaging : canvas types, SpriteDefinition, SpriteInstance

---
+++ sporniket-game-api/src/main/java/com/sporniket/libre/game/api/canvas/BoxCopyMachine.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.sporniket.libre.game.api.canvas;
+
+/**
+ * Define the service allowing to copy a box from a canvas to another one.
+ * 
+ * Canvas are arbitrarily identified by an integer, usually an index.
+ * 
+ * @author dsporn
+ *
+ */
+public interface BoxCopyMachine
+{
+	/**
+	 * Copy a bloc from a canvas to another one.
+	 * 
+	 * @param canvasIdFrom
+	 *            id of the source canvas.
+	 * @param boxSpecs
+	 *            definition of the bloc to copy in the source canvas.
+	 * @param canvasIdTo
+	 *            id of the destination canvas.
+	 * @param position
+	 *            position of the top-left corner in the destination canvas.
+	 */
+	void copy(int canvasIdFrom, Box boxSpecs, int canvasIdTo, Point position);
+}
---
+++ sporniket-game-api/src/main/java/com/sporniket/libre/game/api/sprite/SpriteDefinition.java
@@ -0,0 +1,166 @@
+package com.sporniket.libre.game.api.sprite;
+
+import java.beans.*;
+import java.util.*;
+
+import com.sporniket.libre.game.api.canvas.Box;
+import com.sporniket.libre.game.api.canvas.Point;
+
+/**
+ * A sprite is basically a box from a source canvas that is positionned on a target canvas using a "hot point"..
+ *
+ * The hot point is relative to the (x,y) corner. When positionning a object described by a Sprite box, the position of this object
+ * is the position of the hot point, thus x and y are computed accordingly. To look up more easily a sprite, it is identifiable by
+ * an identifier.
+ * 
+ * LGPL v3
+ * 
+ *
+ * @version 15.05.01-SNAPSHOT
+ * @author David SPORN
+ */
+public class SpriteDefinition
+{
+	/**
+	 * Property "hotPoint" : .
+	 */
+	private com.sporniket.libre.game.api.canvas.Point myHotPoint;
+
+	/**
+	 * Property "id" : .
+	 */
+	private String myId;
+
+	/**
+	 * Property "sourceBox" : .
+	 */
+	private com.sporniket.libre.game.api.canvas.Box mySourceBox;
+
+	/**
+	 * Read accessor for property "hotPoint" : .
+	 *
+	 * 
+	 *
+	 * @return Property "hotPoint".
+	 *
+	 */
+	public com.sporniket.libre.game.api.canvas.Point getHotPoint()
+	{
+		return myHotPoint;
+	}
+
+	/**
+	 * Read accessor for property "id" : .
+	 *
+	 * 
+	 *
+	 * @return Property "id".
+	 *
+	 */
+	public String getId()
+	{
+		return myId;
+	}
+
+	/**
+	 * Read accessor for property "sourceBox" : .
+	 *
+	 * 
+	 *
+	 * @return Property "sourceBox".
+	 *
+	 */
+	public com.sporniket.libre.game.api.canvas.Box getSourceBox()
+	{
+		return mySourceBox;
+	}
+
+	/**
+	 * Write accessor for property "hotPoint" : .
+	 *
+	 * 
+	 *
+	 * @param hotPoint
+	 *            Value of property "hotPoint".
+	 *
+	 */
+	public void setHotPoint(com.sporniket.libre.game.api.canvas.Point hotPoint)
+	{
+		myHotPoint = hotPoint;
+	}
+
+	/**
+	 * Write accessor for property "id" : .
+	 *
+	 * 
+	 *
+	 * @param id
+	 *            Value of property "id".
+	 *
+	 */
+	public void setId(String id)
+	{
+		myId = id;
+	}
+
+	/**
+	 * Write accessor for property "sourceBox" : .
+	 *
+	 * 
+	 *
+	 * @param sourceBox
+	 *            Value of property "sourceBox".
+	 *
+	 */
+	public void setSourceBox(com.sporniket.libre.game.api.canvas.Box sourceBox)
+	{
+		mySourceBox = sourceBox;
+	}
+
+	/**
+	 * Fluent write accessor for property "hotPoint" : .
+	 *
+	 * 
+	 *
+	 * @param hotPoint
+	 *            Value of property "hotPoint".
+	 *
+	 */
+	public SpriteDefinition withHotPoint(com.sporniket.libre.game.api.canvas.Point hotPoint)
+	{
+		setHotPoint(hotPoint);
+		return this;
+	}
+
+	/**
+	 * Fluent write accessor for property "id" : .
+	 *
+	 * 
+	 *
+	 * @param id
+	 *            Value of property "id".
+	 *
+	 */
+	public SpriteDefinition withId(String id)
+	{
+		setId(id);
+		return this;
+	}
+
+	/**
+	 * Fluent write accessor for property "sourceBox" : .
+	 *
+	 * 
+	 *
+	 * @param sourceBox
+	 *            Value of property "sourceBox".
+	 *
+	 */
+	public SpriteDefinition withSourceBox(com.sporniket.libre.game.api.canvas.Box sourceBox)
+	{
+		setSourceBox(sourceBox);
+		return this;
+	}
+
+}
+// Generation date : 2016-08-04 22:16:42.865.

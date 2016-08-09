fa734c27dd13fa58e118d0b6ff5df77f151d3d14
David SPORN <sporniket.studio@gmail.com>
2016-08-09 07:03:08 +0200

    create sprite from grids

--- sporniket-game-api/src/main/java/com/sporniket/libre/game/api/sprite/SpriteDefinitionUtils.java
+++ sporniket-game-api/src/main/java/com/sporniket/libre/game/api/sprite/SpriteDefinitionUtils.java
@@ -3,6 +3,9 @@
  */
 package com.sporniket.libre.game.api.sprite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.sporniket.libre.game.api.canvas.Box;
 import com.sporniket.libre.game.api.canvas.BoxCopyMachine;
 import com.sporniket.libre.game.api.canvas.Point;
@@ -83,5 +86,76 @@ public abstract class SpriteDefinitionUtils
 	public static SpriteDefinition createSprite(String id, int left, int top, int width, int height, int hotX, int hotY)
 	{
 		return createSprite(left, top, width, height, hotX, hotY).withId(id);
+	}
+
+	/**
+	 * Create a set of anonymous {@link SpriteDefinition} that form a regular grid.
+	 * 
+	 * @param gridColspan
+	 *            number of columns of the grid.
+	 * @param gridRowspan
+	 *            number of rows of the grid.
+	 * @param left
+	 *            x coordinate of the top-left corner of the grid.
+	 * @param top
+	 *            y coordinate of the top-left corner of the grid.
+	 * @param width
+	 *            width of one sprite bloc.
+	 * @param height
+	 *            height of one sprite bloc.
+	 * @param hotX
+	 *            x coordinate of the hot point of a sprite.
+	 * @param hotY
+	 *            y coordinate of the hot point of a sprite.
+	 * @return a list of sprite definitions.
+	 */
+	public static List<SpriteDefinition> createSpritesFromGrid(int gridColspan, int gridRowspan, int left, int top, int width,
+			int height, int hotX, int hotY)
+	{
+		List<SpriteDefinition> _result = new ArrayList<SpriteDefinition>(gridColspan * gridRowspan);
+		int _top = top;
+		for (int _row = 0; _row < gridRowspan; _row++)
+		{
+			int _left = left;
+			for (int _col = 0; _col < gridColspan; _col++)
+			{
+				SpriteDefinition _sprite = createSprite(_left, _top, width, height, hotX, hotY);
+				_result.add(_sprite);
+				_left += width;
+			}
+			_top += height;
+		}
+		return _result;
+	}
+
+	/**
+	 * Create a set of {@link SpriteDefinition} that form a regular grid, the first sprite is given an id.
+	 * 
+	 * @param firstSpriteId
+	 *            the id of the first sprite.
+	 * @param gridColspan
+	 *            number of columns of the grid.
+	 * @param gridRowspan
+	 *            number of rows of the grid.
+	 * @param left
+	 *            x coordinate of the top-left corner of the grid.
+	 * @param top
+	 *            y coordinate of the top-left corner of the grid.
+	 * @param width
+	 *            width of one sprite bloc.
+	 * @param height
+	 *            height of one sprite bloc.
+	 * @param hotX
+	 *            x coordinate of the hot point of a sprite.
+	 * @param hotY
+	 *            y coordinate of the hot point of a sprite.
+	 * @return a list of sprite definitions.
+	 */
+	public static List<SpriteDefinition> createSpritesFromGrid(String firstSpriteId, int gridColspan, int gridRowspan, int left,
+			int top, int width, int height, int hotX, int hotY)
+	{
+		List<SpriteDefinition> _result = createSpritesFromGrid(gridColspan, gridRowspan, left, top, width, height, hotX, hotY);
+		_result.get(0).setId(firstSpriteId);
+		return _result;
 	}
 }

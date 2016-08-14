410dd586c82fc57c89e7e4178770c65b2a0d91e8
David SPORN <sporniket.studio@gmail.com>
2016-08-15 00:47:19 +0200

    Canvas view swing component

---
+++ sporniket-game-api/src/main/java/com/sporniket/libre/game/canvas/swing/CanvasView.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package com.sporniket.libre.game.canvas.swing;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+
+import com.sporniket.libre.game.canvas.Box;
+import com.sporniket.libre.game.canvas.Point;
+
+/**
+ * View to link to a canvas id and the canvas manager hosting the canvas.
+ * 
+ * @author dsporn
+ *
+ */
+public class CanvasView extends JComponent
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6032724799458153065L;
+
+	public int myCanvasId;
+
+	private BufferedImagesManager myCanvasManager;
+
+	private final Box myDestinationBox = new Box();
+
+	private final Point myDestinationPoint = new Point();
+
+	/**
+	 * 
+	 */
+	public CanvasView()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCanvasId()
+	{
+		return myCanvasId;
+	}
+
+	public BufferedImagesManager getCanvasManager()
+	{
+		return myCanvasManager;
+	}
+
+	@Override
+	public Dimension getPreferredSize()
+	{
+		Dimension _result = null;
+		if (null != getCanvasManager())
+		{
+			_result = new Dimension(getCanvasManager().getScreenWidth(), getCanvasManager().getScreenHeight());
+		}
+		return _result;
+	}
+
+	public void setCanvasId(int canvasId)
+	{
+		myCanvasId = canvasId;
+	}
+
+	public void setCanvasManager(BufferedImagesManager canvasManager)
+	{
+		myCanvasManager = canvasManager;
+		getDestinationBox().withWidth(myCanvasManager.getScreenWidth()).withHeight(myCanvasManager.getScreenHeight());
+	}
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		Graphics2D _g2 = (Graphics2D) g;
+		getCanvasManager().copyToGraphics(getCanvasId(), getDestinationBox(), _g2, getDestinationPoint(), this);
+	}
+
+	private Box getDestinationBox()
+	{
+		return myDestinationBox;
+	}
+
+	private Point getDestinationPoint()
+	{
+		return myDestinationPoint;
+	}
+}

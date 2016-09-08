9fb8b75347babbb24293d7e38070c6a940b2b68b
David SPORN <sporniket.studio@gmail.com>
2016-09-08 06:37:14 +0200

    allow to specify an image observer to watch for image drawing in BufferedImagesManager

--- sporniket-game-api/src/main/java/com/sporniket/libre/game/canvas/swing/BufferedImagesManager.java
+++ sporniket-game-api/src/main/java/com/sporniket/libre/game/canvas/swing/BufferedImagesManager.java
@@ -15,11 +15,14 @@ import com.sporniket.libre.game.canvas.Point;
 
 /**
  * The swing implementation of {@link CanvasManager} uses {@link BufferedImage}.
+ * 
  * @author dsporn
  *
  */
 public class BufferedImagesManager extends CanvasManager<BufferedImage> implements ImageObserver
 {
+	private ImageObserver myImageObserver = this;
+
 	public BufferedImagesManager(int screenWidth, int screenHeight)
 	{
 		super(screenWidth, screenHeight);
@@ -28,33 +31,56 @@ public class BufferedImagesManager extends CanvasManager<BufferedImage> implemen
 	@Override
 	public void copy(int canvasIdFrom, Box boxSpecs, int canvasIdTo, Point position)
 	{
-		Graphics2D _to = getCanvasRegistry().get(canvasIdTo).getCanvas().createGraphics() ;
-		_to.setComposite(AlphaComposite.SrcOver) ;
-		
-		copyToGraphics(canvasIdFrom, boxSpecs, _to, position, this);
+		Graphics2D _to = getCanvasRegistry().get(canvasIdTo).getCanvas().createGraphics();
+		_to.setComposite(AlphaComposite.SrcOver);
+
+		copyToGraphics(canvasIdFrom, boxSpecs, _to, position, getImageObserver());
 	}
 
 	@Override
 	public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height)
 	{
-		//do nothing
+		// do nothing
 		return false;
 	}
 
 	@Override
 	public void replace(int canvasIdFrom, Box boxSpecs, int canvasIdTo, Point position)
 	{
-		Graphics2D _to = getCanvasRegistry().get(canvasIdTo).getCanvas().createGraphics() ;
-		_to.setComposite(AlphaComposite.Src) ;
-		
-		copyToGraphics(canvasIdFrom, boxSpecs, _to, position, this);
+		Graphics2D _to = getCanvasRegistry().get(canvasIdTo).getCanvas().createGraphics();
+		_to.setComposite(AlphaComposite.Src);
+
+		copyToGraphics(canvasIdFrom, boxSpecs, _to, position, getImageObserver());
 	}
-	
+
+	/**
+	 * If one has an ImageObserver interested, use this.
+	 * 
+	 * @param imageObserver
+	 *            the image observer.
+	 * @return this manager.
+	 */
+	public BufferedImagesManager withImageObserver(ImageObserver imageObserver)
+	{
+		setImageObserver(imageObserver);
+		return this;
+	}
+
 	@Override
 	protected BufferedImage createCanvasData(int width, int height)
 	{
 		BufferedImage _image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
 		return _image;
+	}
+
+	private ImageObserver getImageObserver()
+	{
+		return myImageObserver;
+	}
+
+	private void setImageObserver(ImageObserver imageObserver)
+	{
+		myImageObserver = imageObserver;
 	}
 
 	void copyToGraphics(int canvasIdFrom, Box boxSpecs, Graphics2D to, Point position, ImageObserver observer)

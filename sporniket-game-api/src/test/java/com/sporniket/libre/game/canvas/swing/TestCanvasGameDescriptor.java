6739d28d5ff451cd19342af188db9e7f54341c06
David SPORN <sporniket.studio@gmail.com>
2016-09-24 07:16:18 +0200

    fixed test on canvas descriptor to follow descriptor modifications

--- sporniket-game-api/src/test/java/com/sporniket/libre/game/canvas/swing/TestCanvasGameDescriptor.java
+++ sporniket-game-api/src/test/java/com/sporniket/libre/game/canvas/swing/TestCanvasGameDescriptor.java
@@ -29,8 +29,8 @@ public class TestCanvasGameDescriptor
 	{
 		CanvasGameDescriptor _descriptor = CanvasGameDescriptorUtils.load("classpath:demo/game/game.properties", Encoding.ISO_8859_1);
 		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForData(), is("classpath:demo/game/assets"));
-		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForPictures(), is("{gdef}-gfx"));
-		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForSprites(), is("{gdef}-sprites"));
+		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForPictures(), is("gfx-{gdef}"));
+		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForSprites(), is("sprites"));
 		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForSoundEffects(), is("sound-fx"));
 		assertThat(_descriptor.getBaseUrlSpecs().getBaseUrlForJukebox(), is("sound-music"));
 		
@@ -42,9 +42,9 @@ public class TestCanvasGameDescriptor
 		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[0], is("screen0:offScreen"));
 		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[1], is("screen1:offScreen"));
 		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[2], is("screen2:offScreen"));
-		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[3], is("tileset:url:xxx.png"));
+		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[3], is("tileset:url:spritesheet.png"));
 		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[4], is("background:urlList:back01.png, back02.png, ..."));
-		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[5], is("textSource:offScreen"));
+		assertThat(_descriptor.getCanvasManagerSpecs().getCanvasSpecs()[5], is("main:offScreen"));
 		
 		assertThat(_descriptor.getCanvasSpecs(), is(not(nullValue())));
 		assertThat(_descriptor.getCanvasSpecs().length, is(2));

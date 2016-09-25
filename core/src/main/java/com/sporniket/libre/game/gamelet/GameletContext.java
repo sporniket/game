aba9c96134ca0c0015c2f6d5c90010635022407b
David SPORN <sporniket.studio@gmail.com>
2016-09-25 07:26:40 +0200

    remove access to legacy GameControllers input

--- core/src/main/java/com/sporniket/libre/game/gamelet/GameletContext.java
+++ core/src/main/java/com/sporniket/libre/game/gamelet/GameletContext.java
@@ -3,13 +3,10 @@ package com.sporniket.libre.game.gamelet;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sporniket.libre.game.gamelet.input.GameControllerStateProvider;
 import com.sporniket.libre.game.input.Pointer;
 
 public class GameletContext
 {
-
-	private GameControllerStateProvider myGameControllers;
 
 	/**
 	 * Store a log of {@link Pointer} recorded since the last call.
@@ -21,24 +18,9 @@ public class GameletContext
 		super();
 	}
 
-	/**
-	 * Give access to the game controllers.
-	 * 
-	 * @return access to the game controller states.
-	 */
-	public GameControllerStateProvider getGameControllers()
-	{
-		return myGameControllers;
-	}
-
 	public List<Pointer> getPointerLog()
 	{
 		return myPointerLog;
-	}
-
-	public void setGameControllers(GameControllerStateProvider gameControllers)
-	{
-		myGameControllers = gameControllers;
 	}
 
 	public void setPointerLog(List<Pointer> newLog)

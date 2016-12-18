fb60f8fb965e20ef194210cf34c2f8db415c9842
David SPORN <sporniket.studio@gmail.com>
2016-12-18 16:37:17 +0100

    specify the 'lifecycle' of a Canvas game descriptor

--- canvas/src/main/java/com/sporniket/libre/game/canvas/descriptor/package-info.java
+++ canvas/src/main/java/com/sporniket/libre/game/canvas/descriptor/package-info.java
@@ -1,6 +1,16 @@
 /**
  * Description of a canvas game.
  * 
+ * The lifecycle of the descriptor is as described here :
+ * 
+ * <ol>
+ * <li>Reading and parsing</li>
+ * <li>Select (user or programmatically) crucial settings like canvas size.</li>
+ * <li>Use selection to compute specific values (e.g. graphical definition)</li>
+ * <li>In the descriptor values (e.g. url bases), substitute any tag (e.g. <code>{gdef}</code>) with the computed value.</li>
+ * <li>The descriptor is ready to use for running the game.</li>
+ * </ol>
+ * 
  * @author dsporn
  *
  */

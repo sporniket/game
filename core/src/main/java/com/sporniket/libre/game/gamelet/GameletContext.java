09252358398c5f58385108276bf69ab518326fe9
David SPORN <sporniket.studio@gmail.com>
2017-01-26 06:49:24 +0100

    closes #8 : a map of value is accessible from the game context

--- core/src/main/java/com/sporniket/libre/game/gamelet/GameletContext.java
+++ core/src/main/java/com/sporniket/libre/game/gamelet/GameletContext.java
@@ -1,7 +1,9 @@
 package com.sporniket.libre.game.gamelet;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.sporniket.libre.game.input.Pointer;
 
@@ -9,13 +11,23 @@ public class GameletContext
 {
 
 	/**
+	 * Storage for value that can be exchanged and/or persisted.
+	 */
+	private final Map<String, Object> myData = new HashMap<>();
+
+	/**
 	 * Store a log of {@link Pointer} recorded since the last call.
 	 */
-	private final List<Pointer> myPointerLog = new ArrayList<Pointer>(50);
+	private final List<Pointer> myPointerLog = new ArrayList<>(50);
 
 	public GameletContext()
 	{
 		super();
+	}
+
+	public Map<String, Object> getData()
+	{
+		return myData;
 	}
 
 	public List<Pointer> getPointerLog()

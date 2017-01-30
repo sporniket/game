3d3613d7ee9e79f2558be1e94345d21a2838ba29
David SPORN <sporniket.studio@gmail.com>
2017-01-30 06:59:51 +0100

    modify gamelet to use values from context data (linked to graphical definition) instead of constants

--- canvas-swing/src/test/java/com/sporniket/libre/game/canvas/swing/demo_00/DemoGamelet.java
+++ canvas-swing/src/test/java/com/sporniket/libre/game/canvas/swing/demo_00/DemoGamelet.java
@@ -24,6 +24,8 @@ import com.sporniket.libre.game.pal.codec.ParsingErrorException;
  */
 public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 {
+	private static final String NAME__GRID_SIZE = "gridSize";
+
 	static final String CANVAS_GUID__BACKGROUND = "main";
 
 	static final String CANVAS_GUID__TILESET = "tileset";
@@ -36,12 +38,18 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 
 	private boolean myDragging;
 
+	private int myGridSize = GRID_SIZE;
+
 	/**
 	 * Store the last id of the offscreen canvas to draw into, to check if it is required to redraw all the tiles.
 	 */
 	private int myLastCanvasId = -1;
 
-	private final int[][] myMap = new int[MAP_ROWS][MAP_COLS];
+	private int[][] myMap ;
+
+	private int myMapCols = MAP_COLS;
+
+	private int myMapRows = MAP_ROWS;
 
 	/**
 	 * delta between the position and the mouse pointer, when dragging.
@@ -59,75 +67,29 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 
 	SpriteDefinition[] myTilePool;
 
-	@Override
-	public void render(CanvasGameletContext<BufferedImage> context, int cidDestination, int cidPreviousRender)
+	private int getGridSize()
 	{
-		CanvasManager<BufferedImage> _canvasManager = context.getCanvasManager();
-		int _cidBackground = _canvasManager.getCanvasId(CANVAS_GUID__BACKGROUND);
-
-		_canvasManager.copy(_cidBackground, _canvasManager.getScreenBox(), cidDestination, _canvasManager.getScreenCornerTopLeft());
-
-		// test transparency mode
-		int _cidTileset = _canvasManager.getCanvasId(CANVAS_GUID__TILESET);
-		_canvasManager.copy(_cidTileset, getSprites().get(2).getSourceBox(), cidDestination, new Point()
-				.withX(getPosition().getX()).withY(getPosition().getY()));
-		_canvasManager.replace(_cidTileset, getSprites().get(2).getSourceBox(), cidDestination,
-				new Point().withX(getPosition().getX() + 64).withY(getPosition().getY()));
-
-	}
-
-	@Override
-	protected void init(CanvasGameletContext<BufferedImage> context) throws GameletException
-	{
-		// "load" the tilemap
-		init__loadMap();
-		try
-		{
-			setSprites(new SpriteDecoder().decode("tileGrid 4x1:0,0,32,32"));
-			List<SpriteDefinition> _sprites = getSprites();
-			setTilePool(new SpriteDefinition[]
-			{
-					_sprites.get(0), _sprites.get(0), _sprites.get(0), _sprites.get(1)
-			});
-
-			CanvasManager<BufferedImage> _canvasManager = context.getCanvasManager();
-			int _cidDisplay = _canvasManager.getCanvasId(CANVAS_GUID__BACKGROUND);
-			if (getLastCanvasId() != _cidDisplay)
-			{
-				regenerateBackground(_canvasManager, _cidDisplay);
-				setLastCanvasId(_cidDisplay);
-			}
-
-			requestRender();
-
-		}
-		catch (ParsingErrorException _exception)
-		{
-			throw new GameletException(_exception);
-		}
-	}
-
-	@Override
-	protected void rewind(CanvasGameletContext<BufferedImage> context) throws GameletException
-	{
-		getPosition().withX(0).withY(0);
-		setDragging(false);
-	}
-
-	@Override
-	protected void run(long elapsedTime, CanvasGameletContext<BufferedImage> context) throws GameletException
-	{
-		// process mouse event (drag using left click only ==> pointer index = 0)
-		boolean _isUpdated = run__processPointers(context);
-		if (_isUpdated)
-		{
-			requestRender();
-		}
+		return myGridSize;
 	}
 
 	private int getLastCanvasId()
 	{
 		return myLastCanvasId;
+	}
+
+	private int[][] getMap()
+	{
+		return myMap;
+	}
+
+	private int getMapCols()
+	{
+		return myMapCols;
+	}
+
+	private int getMapRows()
+	{
+		return myMapRows;
 	}
 
 	private Point getPointerDelta()
@@ -155,6 +117,57 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 		return myTilePool;
 	}
 
+	@Override
+	protected void init(CanvasGameletContext<BufferedImage> context) throws GameletException
+	{
+		//init rendering parameters
+		init__renderingParameters(context);
+		
+		// "load" the tilemap
+		init__loadMap();
+		try
+		{
+			setSprites(new SpriteDecoder().decode("tileGrid 4x1:0,0,32,32"));
+			List<SpriteDefinition> _sprites = getSprites();
+			setTilePool(new SpriteDefinition[]
+			{
+					_sprites.get(0),
+					_sprites.get(0),
+					_sprites.get(0),
+					_sprites.get(1)
+			});
+
+			CanvasManager<BufferedImage> _canvasManager = context.getCanvasManager();
+			int _cidDisplay = _canvasManager.getCanvasId(CANVAS_GUID__BACKGROUND);
+			if (getLastCanvasId() != _cidDisplay)
+			{
+				regenerateBackground(_canvasManager, _cidDisplay);
+				setLastCanvasId(_cidDisplay);
+			}
+
+			requestRender();
+
+		}
+		catch (ParsingErrorException _exception)
+		{
+			throw new GameletException(_exception);
+		}
+	}
+
+	private void init__renderingParameters(CanvasGameletContext<BufferedImage> context)
+	{
+		setGridSize((int) context.getData().get(NAME__GRID_SIZE));
+		int _screenWidth = context.getCanvasManager().getScreenWidth();
+		int _mapCols = _screenWidth/getGridSize();
+		if (0 != _screenWidth % _mapCols)
+		{
+			_mapCols++;
+		}
+		setMapCols(_mapCols);
+		setMapRows(getMapCols());
+		setMap(new int[getMapRows()][getMapCols()]);
+	}
+
 	/**
 	 * 
 	 */
@@ -162,11 +175,14 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 	{
 		int[] _valueMap =
 		{
-				0, 0, 0, 1
+				0,
+				0,
+				0,
+				1
 		}; // ratio : 3 background tiles for one wall tile
-		for (int _row = 0; _row < MAP_ROWS; _row++)
+		for (int _row = 0; _row < getMapRows(); _row++)
 		{
-			for (int _col = 0; _col < MAP_COLS; _col++)
+			for (int _col = 0; _col < getMapCols(); _col++)
 			{
 				myMap[_row][_col] = getRand().nextInt(_valueMap.length);
 			}
@@ -191,8 +207,8 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 		Point _to = new Point();
 		int _y = 0;
 		_to.setY(_y);
-		int _colspan = canvasManager.getScreenWidth() / GRID_SIZE + 1;
-		int _rowspan = canvasManager.getScreenHeight() / GRID_SIZE + 1;
+		int _colspan = getMapCols();
+		int _rowspan = getMapRows();
 		for (int _row = 0; _row < _rowspan; _row++)
 		{
 			int _x = 0;
@@ -202,11 +218,46 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 				int _sprite = myMap[_row][_col];
 				SpriteDefinitionUtils.copySpriteBloc(getTilePool()[_sprite], _to, (BoxCopyMachine) canvasManager, _cidTileset,
 						cidBackground);
-				_x += GRID_SIZE;
+				_x += getGridSize();
 				_to.setX(_x);
 			}
-			_y += GRID_SIZE;
+			_y += getGridSize();
 			_to.setY(_y);
+		}
+	}
+
+	@Override
+	public void render(CanvasGameletContext<BufferedImage> context, int cidDestination, int cidPreviousRender)
+	{
+		CanvasManager<BufferedImage> _canvasManager = context.getCanvasManager();
+		int _cidBackground = _canvasManager.getCanvasId(CANVAS_GUID__BACKGROUND);
+
+		_canvasManager.copy(_cidBackground, _canvasManager.getScreenBox(), cidDestination, _canvasManager.getScreenCornerTopLeft());
+
+		// test transparency mode
+		int _cidTileset = _canvasManager.getCanvasId(CANVAS_GUID__TILESET);
+		_canvasManager.copy(_cidTileset, getSprites().get(2).getSourceBox(), cidDestination,
+				new Point().withX(getPosition().getX()).withY(getPosition().getY()));
+		_canvasManager.replace(_cidTileset, getSprites().get(2).getSourceBox(), cidDestination,
+				new Point().withX(getPosition().getX() + 64).withY(getPosition().getY()));
+
+	}
+
+	@Override
+	protected void rewind(CanvasGameletContext<BufferedImage> context) throws GameletException
+	{
+		getPosition().withX(0).withY(0);
+		setDragging(false);
+	}
+
+	@Override
+	protected void run(long elapsedTime, CanvasGameletContext<BufferedImage> context) throws GameletException
+	{
+		// process mouse event (drag using left click only ==> pointer index = 0)
+		boolean _isUpdated = run__processPointers(context);
+		if (_isUpdated)
+		{
+			requestRender();
 		}
 	}
 
@@ -222,17 +273,16 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 		boolean _isUpdated = false;
 		for (Pointer _pointer : context.getPointerLog())
 		{
-			if (0 == _pointer.getIndex()
-					&& (_pointer.getState() == com.sporniket.libre.game.input.Pointer.State.PRESSED
-							|| _pointer.getState() == com.sporniket.libre.game.input.Pointer.State.NOT_PRESSED || _pointer
-							.getState() == com.sporniket.libre.game.input.Pointer.State.UNDEFINED))
+			if (0 == _pointer.getIndex() && (_pointer.getState() == com.sporniket.libre.game.input.Pointer.State.PRESSED
+					|| _pointer.getState() == com.sporniket.libre.game.input.Pointer.State.NOT_PRESSED
+					|| _pointer.getState() == com.sporniket.libre.game.input.Pointer.State.UNDEFINED))
 			{
 				// update dragging state
 				if (!isDragging() && _pointer.getState() == com.sporniket.libre.game.input.Pointer.State.PRESSED)
 				{
 					int _deltaX = _pointer.getX() - getPosition().getX();
 					int _deltaY = _pointer.getY() - getPosition().getY();
-					if (_deltaX >= 0 && _deltaX < GRID_SIZE && _deltaY >= 0 && _deltaY < GRID_SIZE)
+					if (_deltaX >= 0 && _deltaX < GRID_SIZE && _deltaY >= 0 && _deltaY < getGridSize())
 					{
 						getPointerDelta().withX(_deltaX).withY(_deltaY);
 						setDragging(true);
@@ -246,8 +296,8 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 				// update position
 				if (isDragging())
 				{
-					getPosition().withX(_pointer.getX() - getPointerDelta().getX()).withY(
-							_pointer.getY() - getPointerDelta().getY());
+					getPosition().withX(_pointer.getX() - getPointerDelta().getX())
+							.withY(_pointer.getY() - getPointerDelta().getY());
 					_isUpdated = true;
 				}
 
@@ -261,9 +311,29 @@ public class DemoGamelet extends DefaultCanvasGamelet<BufferedImage>
 		myDragging = dragging;
 	}
 
+	private void setGridSize(int gridSize)
+	{
+		myGridSize = gridSize;
+	}
+
 	private void setLastCanvasId(int lastCanvasId)
 	{
 		myLastCanvasId = lastCanvasId;
+	}
+
+	private void setMap(int[][] map)
+	{
+		myMap = map;
+	}
+
+	private void setMapCols(int mapCols)
+	{
+		myMapCols = mapCols;
+	}
+
+	private void setMapRows(int mapRows)
+	{
+		myMapRows = mapRows;
 	}
 
 	private void setSprites(List<SpriteDefinition> sprites)
